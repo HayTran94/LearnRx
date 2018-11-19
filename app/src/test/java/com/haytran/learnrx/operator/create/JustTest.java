113dc8b086bc99888653f494627e3fd8e113e6a2
Hay Tran <haytran@fiot.vn>
2018-11-19 10:12:35 +0700

    update doOn-Action

--- app/src/test/java/com/haytran/learnrx/operator/create/JustTest.java
+++ app/src/test/java/com/haytran/learnrx/operator/create/JustTest.java
@@ -6,6 +6,7 @@ import com.haytran.learnrx.Movie;
 import org.junit.Test;
 
 import io.reactivex.Observable;
+import io.reactivex.functions.Consumer;
 
 public class JustTest extends BaseTest {
 
@@ -20,12 +21,18 @@ public class JustTest extends BaseTest {
     @Test
     public void test2() {
         Observable observable = Observable.just(1, 2, 3, 4);
-        observable.subscribe(getObserver());
+        observable
+                .doOnNext((o) -> System.out.println("doOnNext " + o))
+                .doAfterNext((o) -> System.out.println("doAfterNext " + o))
+                .doOnComplete(() -> System.out.println("doOnComplete " ))
+                .doOnSubscribe((o) -> System.out.println("doOnSubscribe " + o))
+                .doOnTerminate(() -> System.out.println("doOnTerminate " ))
+                .subscribe(getObserver());
     }
 
     @Test
     public void test3() {
-        int[] ints = new int[]{1,2,3,4};
+        int[] ints = new int[]{1, 2, 3, 4};
         Observable observable = Observable.just(ints);
         observable.subscribe(getObserver());
     }
